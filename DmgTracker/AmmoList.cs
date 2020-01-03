455565420a337f0f76e3e2de82acb894c19f1cbd
JC09 <32849971+JC009@users.noreply.github.com>
2020-01-03 12:51:12 +0100

    Added AmmoList enum
    Ammunition class will go away, probably

---
+++ DmgTracker/AmmoList.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmgTracker
+{
+    enum MM9
+    {
+        PrcShort = 0,
+        PrcMedium = -2,
+        PrcLong = -6,
+
+        ApShort = 2,
+        ApMedium = 2,
+        ApLong = 0,
+
+        DmgShort = 3,
+        DmgMedium = 3,
+        DmgLong = 2,
+    }
+
+    enum ACP45
+    {
+        PrcShort = 0,
+        PrcMedium = -3,
+        PrcLong = -7,
+
+        ApShort = 2,
+        ApMedium = 2,
+        ApLong = 0,
+
+        DmgShort = 4,
+        DmgMedium = 4,
+        DmgLong = 2
+    }
+
+    enum MM10
+    {
+        PrcShort = 0,
+        PrcMedium = -2,
+        PrcLong = -6,
+
+        ApShort = 2,
+        ApMedium = 2,
+        ApLong = 0,
+
+        DmgShort = 4,
+        DmgMedium = 4,
+        DmgLong = 2
+    }
+
+    enum AE50
+    {
+        PrcShort = 0,
+        PrcMedium = -2,
+        PrcLong = -5,
+
+        ApShort = 4,
+        ApMedium = 4,
+        ApLong = 1,
+
+        DmgShort = 8,
+        DmgMedium = 7,
+        DmgLong = 5
+    }
+
+    enum MAG357
+    {
+        PrcShort = 0,
+        PrcMedium = -2,
+        PrcLong = -6,
+
+        ApShort = 3,
+        ApMedium = 3,
+        ApLong = 1,
+
+        DmgShort = 5,
+        DmgMedium = 5,
+        DmgLong = 4
+    }
+
+    enum MAG44
+    {
+        PrcShort = 0,
+        PrcMedium = -2,
+        PrcLong = -6,
+
+        ApShort = 4,
+        ApMedium = 4,
+        ApLong = 2,
+
+        DmgShort = 6,
+        DmgMedium = 6, 
+        DmgLong = 5
+    }
+
+    enum MAG500
+    {
+        PrcShort = 0,
+        PrcMedium = -2,
+        PrcLong = -5,
+
+        ApShort = 5,
+        ApMedium = 5,
+        ApLong = 2,
+
+        DmgShort = 10,
+        DmgMedium = 9,
+        DmgLong = 8
+    }
+
+    enum MM556
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = -1,
+
+        ApShort = 4,
+        ApMedium = 4,
+        ApLong = 4,
+
+        DmgShort = 4,
+        DmgMedium = 4,
+        DmgLong = 4
+    }
+
+    enum GRENDEL
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = -3,
+
+        ApShort = 4,
+        ApMedium = 4,
+        ApLong = 3,
+
+        DmgShort = 5,
+        DmgMedium = 5,
+        DmgLong = 5
+    }
+
+    enum MM762
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = -1,
+
+        ApShort = 5,
+        ApMedium = 5,
+        ApLong = 5,
+
+        DmgShort = 5,
+        DmgMedium = 5,
+        DmgLong = 5
+    }
+
+    enum BLACKOUT
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = -4,
+
+        ApShort = 4,
+        ApMedium = 3,
+        ApLong = 3,
+
+        DmgShort = 4,
+        DmgMedium = 4,
+        DmgLong = 3
+    }
+
+    enum WINMAG
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = 0,
+
+        ApShort = 7,
+        ApMedium = 7,
+        ApLong = 7,
+
+        DmgShort = 10,
+        DmgMedium = 10,
+        DmgLong = 10
+    }
+
+    enum LAPMAG
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = 0,
+
+        ApShort = 8,
+        ApMedium = 8,
+        ApLong = 8,
+
+        DmgShort = 14,
+        DmgMedium = 14,
+        DmgLong = 14
+    }
+
+    enum BMG
+    {
+        PrcShort = 0,
+        PrcMedium = 0,
+        PrcLong = 0,
+
+        ApShort = 15,
+        ApMedium = 15,
+        ApLong = 15,
+
+        DmgShort = 30,
+        DmgMedium = 30,
+        DmgLong = 30
+    }
+
+    enum GAUGE12LEAD
+    {
+        PrcShort = 2,
+        PrcMedium = -2,
+        PrcLong = -7,
+
+        ApShort = 1,
+        ApMedium = 1,
+        ApLong = 0,
+
+        DmgShort = 2,
+        DmgMedium = 2,
+        DmgLong = 2,
+
+        ShotNumberShort = 6,
+        ShotNumberMedium = 4,
+        ShotNumberLong = 2,
+    }
+}
---
+++ DmgTracker/Ammunition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DmgTracker
+{
+    class Ammunition
+    {
+        public int PrcShort { get; set; }
+        public int PrcMedium { get; set; }
+        public int PrcLong { get; set; }
+        public int ApShort { get; set; }
+        public int ApMedium { get; set; }
+        public int ApLong { get; set; }
+        public int DmgShort { get; set; }
+        public int DmgMedium { get; set; }
+        public int DmgLong { get; set; }
+
+        public Ammunition(int prcShort, int prcMedium, int prcLong, 
+            int apShort, int apMedium, int apLong, 
+            int dmgShort, int dmgMedium, int dmgLong)
+        {
+            PrcShort = prcShort;
+            PrcMedium = prcMedium;
+            PrcLong = prcLong;
+
+            ApShort = apShort;
+            ApMedium = apMedium;
+            ApLong = apLong;
+
+            DmgShort = dmgShort;
+            DmgMedium = dmgMedium;
+            DmgLong = dmgLong;
+        }
+    }
+}
